18248b81259b2b1d64f999e16386723cad73b622
linkoad <danteabreu1@gmail.com>
2019-05-27 01:26:42 +0000

    completing-implementation-blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,11 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
     
-    def delete
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        redirect_to posts_path
     end
     
     private
