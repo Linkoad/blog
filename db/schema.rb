738d1166c39ae397359b96c08225c4bbc816a8e3
linkoad <danteabreu1@gmail.com>
2019-05-27 19:12:31 +0000

    Add comments-in-posts-users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_172754) do
+ActiveRecord::Schema.define(version: 2019_05_27_015145) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
