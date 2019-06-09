d0fb346b8299ba1b396a2292f2d6288b7c77efeb
Linkoad <danteabreu1@gmail.com>
2019-06-09 21:24:52 +0000

    Add authenticate-user.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,53 @@
 class PostsController < ApplicationController
-    before_action :find_post, only: [:show, :update, :edit, :destroy]
-    
-    def index
-        @post = Post.all.order("created_at DESC")
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @post = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
-    
-    def new
-        @post = Post.new
+  end
+
+  def show
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
-    
-    def create
-       @post = Post.new(post_params)
-       
-       if @post.save
-           redirect_to @post
-       else
-           render 'new'
-       end
-    end
-    
-    def show
-    end
-    
-    def update
-        if @post.update(post_params)
-            redirect_to @post
-        else
-            render 'edit'
-        end
-    end
-    
-    def edit
-    end
-    
-    def destroy
-        @post.destroy
-        
-        redirect_to posts_path
-    end
-    
-    private
-    
-    def post_params
-        params.require(:post).permit(:title, :content)
-    end
-        
-    def find_post
-        @post = Post.find(params[:id])
-    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
 end
